41b15d7ed28495c0b58d388c555b6a7e4cc0d0c0
Fatma Türkmen <fatmaturkmen244@gmail.com>
2026-06-07 22:01:59 +0300

    feat(frontend): Navbar bileseni App.jsx genel yerlesimine entegre edildi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { PublicRoute, PrivateRoute } from './components/RouteGuard';
+import Navbar from './components/Navbar';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
@@ -10,19 +11,24 @@ export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          {/* Public rotalar */}
-          <Route path="/giris"  element={<PublicRoute><LoginPage /></PublicRoute>} />
-          <Route path="/kayit"  element={<PublicRoute><RegisterPage /></PublicRoute>} />
-          <Route path="/sifremi-unuttum" element={<ForgotPasswordPage />} />
+        <div className="app-layout">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              {/* Public rotalar */}
+              <Route path="/giris"  element={<PublicRoute><LoginPage /></PublicRoute>} />
+              <Route path="/kayit"  element={<PublicRoute><RegisterPage /></PublicRoute>} />
+              <Route path="/sifremi-unuttum" element={<ForgotPasswordPage />} />
 
-          {/* Private rotalar */}
-          <Route path="/profil" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+              {/* Private rotalar */}
+              <Route path="/profil" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
 
-          {/* Yönlendirme */}
-          <Route path="/" element={<Navigate to="/giris" replace />} />
-          <Route path="*" element={<Navigate to="/giris" replace />} />
-        </Routes>
+              {/* Yönlendirme */}
+              <Route path="/" element={<Navigate to="/giris" replace />} />
+              <Route path="*" element={<Navigate to="/giris" replace />} />
+            </Routes>
+          </main>
+        </div>
       </BrowserRouter>
     </AuthProvider>
   );
